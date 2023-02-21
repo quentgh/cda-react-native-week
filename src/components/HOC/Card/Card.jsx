6bc219e7ef37f6baaf6d7bacb8a8fc751f1124f4
quentgh <>
2023-02-21 12:39:38 +0100

    add login and sign up composant without form

--- src/components/HOC/Card/Card.jsx
+++ src/components/HOC/Card/Card.jsx
@@ -3,40 +3,35 @@ import { StyleSheet, View, Text } from "react-native";
 export default function Card(props) {
   return (
     <View style={cardStyle.container}>
-      <Text
-        style={{
-          color: "#eee",
-          fontSize: 30,
-        }}
-      >
-        {props.title}
-      </Text>
-      <Text
-        style={{
-          margin: 10,
-          color: "#eee",
-        }}
-      >
-        {props.content}
-      </Text>
-      <View
-        style={{
-          margin: 20,
-        }}
-      >
-        <Text>{props.children}</Text>
-      </View>
+      <Text style={cardStyle.title}>{props.title}</Text>
+      <Text style={cardStyle.content}>{props.content}</Text>
+      <View style={cardStyle.children}>{props.children}</View>
     </View>
   );
 }
 
+//Styles
 const cardStyle = StyleSheet.create({
   container: {
-    width: "100%",
-    padding: 20,
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    color: "#eee",
+    backgroundColor: "royalblue",
+    borderRadius: 10,
+    width: "90%",
+    alignSelf: "center",
+  },
+
+  title: {
+    color: "whitesmoke",
+    fontSize: 35,
+    textAlign: "center",
+  },
+  content: {
+    color: "whitesmoke",
+    fontSize: 20,
+    textAlign: "center",
+    padding: 5,
+  },
+  children: {
+    padding: 10,
+    backgroundColor: "whitesmoke",
   },
 });
--- src/components/pages/Auth/Auth.jsx
+++ src/components/pages/Auth/Auth.jsx
@@ -1,21 +1,42 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
 import Card from "../../HOC/Card/Card";
+import LoginForm from "../../LoginForm/LoginForm";
+import SignUpForm from "../../SignUpForm/SignUpForm";
+import { StyleSheet, View, Text } from "react-native";
 
-let card = {
+const card = {
   title: "Bienvenu !",
   content: "Veuillez vous authentifier",
-  children: "Un composant Card",
 };
 
 export default function Auth() {
+  const [login, isLogin] = useState(true);
+
+  function toggleLogin() {
+    isLogin(!login);
+  }
+
   return (
     <View>
-      {/* <Text>Authentification</Text> */}
-      <Card
-        title={card.title}
-        content={card.content}
-        children={card.children}
-      ></Card>
+      <Card title={card.title} content={card.content}>
+        <View>
+        {login ? <LoginForm /> : <SignUpForm />}
+        <TouchableOpacity style={styles.button} onPress={toggleLogin}>
+          <Text>Pas encore inscrit ?</Text>
+        </TouchableOpacity>
+      </View>
+      </Card>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    backgroundColor: "#ddd",
+    margin:20,
+    padding: 10,
+    color: "#333",
+  },
+});
